fd881f086f4325e7e7f7f8197b48dd77ecd0b422
Nikitha-gr <nikitha14.gr@gmail.com>
2024-12-08 23:08:14 +0530

    Question No.12 - Square & Cube

---
+++ ES6_Assignments/q12.js
@@ -0,0 +1,10 @@
+//Square and Cube of a number with Implicit Return
+//Write an arrow function that takes a number as input and returns both its square and cube in an object,
+//using implicit return. The function should be as concise as possible.
+//Requirements:
+//The arrow function should use implicit return (no curly braces or return keyword).
+//The function should return an object in the format { square, cube }.
+
+let sqrCube = (num)=>({square:(num*num),cube:(num*num*num)});
+
+console.log(sqrCube(3));
